898f1c8807cf4a025a83f529b728d171fd4a2327
Pippin Williamson <pippin@pippinsplugins.com>
2014-03-23 20:08:38 -0500

    update affwp_get_affiliate_rate() to use the rate set in settings.

--- includes/affiliate-functions.php
+++ includes/affiliate-functions.php
@@ -42,7 +42,7 @@ function affwp_set_affiliate_status( $affiliate, $status = '' ) {
 function affwp_get_affiliate_rate( $affiliate_id = 0 ) {
 
 	// default rate
-	$rate = 30;
+	$rate = affiliate_wp()->settings->get( 'referral_rate', 30 );
 
 	$affiliate_rate = affiliate_wp()->affiliates->get_column( 'rate', $affiliate_id );
 
