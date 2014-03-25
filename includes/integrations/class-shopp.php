30e1a4fc43214137576b6eaaea0e19a8d5b4a2b7
Pippin Williamson <pippin@pippinsplugins.com>
2014-03-24 20:00:35 -0500

    Base integration for Shopp. #30

--- includes/integrations/class-shopp.php
+++ includes/integrations/class-shopp.php
@@ -2,5 +2,32 @@
 
 class Affiliate_WP_Shopp extends Affiliate_WP_Base {
 	
+	public function init() {
+		add_action( '', array( $this, 'add_pending_referral' ), 10 );
+		add_action( '', array( $this, 'mark_referral_complete' ), 10 );
+	}
+
+	public function add_pending_referral( $order_id = 0 ) {
+
+
+		if( $this->was_referred() ) {
+
+			$this->insert_pending_referral( $amount, $order_id );
+		}
+
+	}
+
+	public function mark_referral_complete( $order_id = 0 ) {
+
+		if( $order->is_transaction_completed() ) {
+
+			$this->complete_referral( $order_id );
+
+		}
+
+		// TODO add order note about referral
+
+	}
 	
 }
+new Affiliate_WP_Shopp;
