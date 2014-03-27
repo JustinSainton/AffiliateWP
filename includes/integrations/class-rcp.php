0063348b7602eb254c3c3d32ed1e5b5bc89dd757
Pippin Williamson <pippin@pippinsplugins.com>
2014-03-26 19:14:58 -0500

    Add (non working) rejection process for deleting RCP payments. #38

--- includes/integrations/class-rcp.php
+++ includes/integrations/class-rcp.php
@@ -8,6 +8,9 @@ class Affiliate_WP_RCP extends Affiliate_WP_Base {
 
 		add_action( 'rcp_form_processing', array( $this, 'add_pending_referral' ), 10, 3 );
 		add_action( 'rcp_insert_payment', array( $this, 'mark_referral_complete' ), 10, 3 );
+		//add_action( 'rcp_delete_payment', array( $this, 'revoke_referral_on_delete' ), 10 );
+
+		add_filter( 'affwp_referral_reference_column', array( $this, 'reference_link' ), 10, 2 );
 	}
 
 	public function add_pending_referral( $post_data, $user_id, $price ) {
@@ -26,6 +29,20 @@ class Affiliate_WP_RCP extends Affiliate_WP_Base {
 
 		$this->complete_referral( $args['subscription_key'] );
 	
+	}
+
+	public function revoke_referral_on_delete( $payment_id = 0 ) {
+
+		// TODO: fix this, it doesn't fire when RCP payments are deleted
+
+		if( ! affiliate_wp()->settings->get( 'revoke_on_refund' ) ) {
+			return;
+		}
+
+		$payments = new RCP_Payments;
+		$payment  = $payments->get_payment( $payment_id );
+		$this->reject_referral( $payment->subscription_key );
+
 	}
 	
 }
