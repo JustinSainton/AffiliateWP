8b5b5c6ddda9a9710566f2e36ba9b9ab9db82a85
Pippin Williamson <pippin@pippinsplugins.com>
2014-03-27 11:21:25 -0500

    Record payment note on pending referral for EDD. #30

--- includes/integrations/class-edd.php
+++ includes/integrations/class-edd.php
@@ -7,7 +7,7 @@ class Affiliate_WP_EDD extends Affiliate_WP_Base {
 		$this->context = 'edd';
 
 		add_action( 'edd_insert_payment', array( $this, 'add_pending_referral' ), 10, 2 );
-		add_action( 'edd_complete_purchase', array( $this, 'complete_referral' ) );
+		add_action( 'edd_complete_purchase', array( $this, 'mark_referral_complete' ) );
 		add_action( 'edd_update_payment_status', array( $this, 'revoke_referral_on_refund' ), 10, 3 );
 		add_action( 'edd_payment_delete', array( $this, 'revoke_referral_on_delete' ), 10 );
 
@@ -17,10 +17,23 @@ class Affiliate_WP_EDD extends Affiliate_WP_Base {
 	public function add_pending_referral( $payment_id = 0, $payment_data = array() ) {
 
 		if( $this->was_referred() ) {
+
 			$this->insert_pending_referral( $payment_data['price'], $payment_id );
+		
+			$referral = affiliate_wp()->referrals->get_by( 'reference', $payment_id, 'edd' );
+			$amount   = affwp_currency_filter( affwp_format_amount( $referral->amount ) );
+			$name     = affiliate_wp()->affiliates->get_affiliate_name( $referral->affiliate_id );
+
+			edd_insert_payment_note( $payment_id, sprintf( __( 'Referral #%d for %s recorded for %s', 'affiliate-wp' ),$referral->referral_id, $amount, $name ) );
 		}
 
 	}
+
+	public function mark_referral_complete( $payment_id = 0 ) {
+
+		$this->complete_referral( $payment_id );
+	}
+
 
 	public function revoke_referral_on_refund( $payment_id = 0, $new_status, $old_status ) {
 	
