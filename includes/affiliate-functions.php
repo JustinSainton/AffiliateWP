f86bdbe76638b005d46b3faf3b21a8b5391321e1
Tunbosun Ayinla <tayinla@gmail.com>
2014-04-01 04:24:02 +0100

    Fixed merge conflict

--- includes/affiliate-functions.php
+++ includes/affiliate-functions.php
@@ -179,13 +179,8 @@ function affwp_get_affiliate_unpaid_earnings( $affiliate, $formatted = false ) {
 
 	$referrals = affiliate_wp()->referrals->get_referrals( array( 'affiliate_id' => $affiliate_id, 'status' => 'unpaid' ) );
 	$earnings = 0;
-<<<<<<< HEAD
-
-	if( ! empty( $earnings ) ) {
-=======
 
 	if( ! empty( $referrals ) ) {
->>>>>>> upstream/master
 
 		foreach( $referrals as $referral ) {
 
