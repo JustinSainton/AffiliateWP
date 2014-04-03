a64978164945a4b6f1287587b9281ba634892d7f
Pippin Williamson <pippin@pippinsplugins.com>
2014-04-02 20:40:50 -0500

    Don't revoke referrals on cancel for PMP

--- includes/integrations/class-pmp.php
+++ includes/integrations/class-pmp.php
@@ -55,7 +55,7 @@ class Affiliate_WP_PMP extends Affiliate_WP_Base {
 			return;
 		}
 
-		if( 'refunded' != $_REQUEST['status'] && 'cancelled' != $_REQUEST['status'] ) {
+		if( 'refunded' != $_REQUEST['status'] ) {
 			return;
 		}
 
