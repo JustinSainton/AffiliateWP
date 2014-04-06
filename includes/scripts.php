3480a7f440005b24645d80bbe93c1ce1d6011ec4
Pippin Williamson <pippin@pippinsplugins.com>
2014-04-05 21:10:57 -0500

    Load .min versions of CSS and JS

--- includes/scripts.php
+++ includes/scripts.php
@@ -3,8 +3,9 @@
 function affwp_admin_scripts( $hook ) {
 
 	// TODO load conditionally
+	$suffix = ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) ? '' : '.min';
 
-	wp_enqueue_script( 'affwp-admin', AFFILIATEWP_PLUGIN_URL . 'assets/js/admin.js', array( 'jquery' ), AFFILIATEWP_VERSION );
+	wp_enqueue_script( 'affwp-admin', AFFILIATEWP_PLUGIN_URL . 'assets/js/admin' . $suffix . '.js', array( 'jquery' ), AFFILIATEWP_VERSION );
 	wp_localize_script( 'affwp-admin', 'affwp_vars', array(
 		'post_id'       => isset( $post->ID ) ? $post->ID : null,
 		'affwp_version' => AFFILIATEWP_VERSION,
@@ -19,8 +20,9 @@ add_action( 'admin_enqueue_scripts', 'affwp_admin_scripts' );
 function affwp_admin_styles( $hook ) {
 
 	// TODO load conditionally
+	$suffix = ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) ? '' : '.min';
 
-	wp_enqueue_style( 'affwp-admin', AFFILIATEWP_PLUGIN_URL . 'assets/css/admin.css', AFFILIATEWP_VERSION );
+	wp_enqueue_style( 'affwp-admin', AFFILIATEWP_PLUGIN_URL . 'assets/css/admin' . $suffix . '.css', AFFILIATEWP_VERSION );
 	wp_enqueue_style( 'dashicons' );
 	$ui_style = ( 'classic' == get_user_option( 'admin_color' ) ) ? 'classic' : 'fresh';
 	wp_enqueue_style( 'jquery-ui-css', AFFILIATEWP_PLUGIN_URL . 'assets/css/jquery-ui-' . $ui_style . '.min.css' );
@@ -35,15 +37,18 @@ function affwp_frontend_styles() {
 		return;
 	}
 
-	if( has_shortcode( $post->post_content, 'affiliate_area' ) ) {
-		wp_enqueue_script( 'affwp-frontend', AFFILIATEWP_PLUGIN_URL . 'assets/js/frontend.js', array( 'jquery' ), AFFILIATEWP_VERSION );
+	if( has_shortcode( $post->post_content, 'affiliate_area' ) || apply_filters( 'affwp_force_frontend_scripts', false ) ) {
+		
+		$suffix = ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) ? '' : '.min';
+
+		wp_enqueue_script( 'affwp-frontend', AFFILIATEWP_PLUGIN_URL . 'assets/js/frontend' . $suffix . '.js', array( 'jquery' ), AFFILIATEWP_VERSION );
 		wp_localize_script( 'affwp-frontend', 'affwp_vars', array(
 			'affwp_version' => AFFILIATEWP_VERSION,
 			'permalinks'    => get_option( 'permalink_structure' ),
 			'currency_sign' => affwp_currency_filter(''),
 			'currency_pos'  => affiliate_wp()->settings->get( 'currency_position', 'before' ),
 		));
-		wp_enqueue_style( 'affwp-forms', AFFILIATEWP_PLUGIN_URL . 'assets/css/forms.css', AFFILIATEWP_VERSION );
+		wp_enqueue_style( 'affwp-forms', AFFILIATEWP_PLUGIN_URL . 'assets/css/forms' . $suffix . '.css', AFFILIATEWP_VERSION );
 	}
 
 }
