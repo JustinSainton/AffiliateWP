e7b4fe133f1a5e63ccfa71fac52bc790fbff35f2
Pippin Williamson <pippin@pippinsplugins.com>
2014-06-30 10:08:33 -0500

    Merge 1.1.3

--- includes/class-integrations.php
+++ includes/class-integrations.php
@@ -12,6 +12,7 @@ class Affiliate_WP_Integrations {
 
 		return apply_filters( 'affwp_integrations', array(
 			'edd'         => 'Easy Digital Downloads',
+			'gravityforms' => 'Gravity Forms',
 			'exchange'    => 'iThemes Exchange',
 			'membermouse' => 'MemberMouse',
 			'jigoshop'    => 'Jigoshop',
@@ -21,6 +22,7 @@ class Affiliate_WP_Integrations {
 			'woocommerce' => 'WooCommerce',
 			'wpec'        => 'WP e-Commerce',
 		) );
+
 	}
 
 	public function get_enabled_integrations() {
