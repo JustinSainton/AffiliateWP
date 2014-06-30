a598f9658edf446a9fb3ba9ae47d66d07d2ce408
Pippin Williamson <pippin@pippinsplugins.com>
2014-06-30 10:53:51 -0500

    First pass at adding download settings for per-product referral rates. #1

--- includes/integrations/class-edd.php
+++ includes/integrations/class-edd.php
@@ -25,6 +25,10 @@ class Affiliate_WP_EDD extends Affiliate_WP_Base {
 		add_action( 'edd_edit_discount_form_bottom', array( $this, 'discount_edit' ) );
 		add_action( 'edd_post_update_discount', array( $this, 'store_discount_affiliate' ), 10, 2 );
 		add_action( 'edd_post_insert_discount', array( $this, 'store_discount_affiliate' ), 10, 2 );
+
+		// Per product referral rates
+		add_action( 'edd_meta_box_settings_fields', array( $this, 'download_settings' ), 100 );
+		add_filter( 'edd_metabox_fields_save', array( $this, 'download_save_fields' ) );
 	}
 
 	/**
@@ -273,6 +277,57 @@ class Affiliate_WP_EDD extends Affiliate_WP_Base {
 		$affiliate_id = affwp_get_affiliate_id( $user_id );
 
 		update_post_meta( $discount_id, 'affwp_discount_affiliate', $affiliate_id );
+	}
+
+	/**
+	 * Adds per-product referral rate settings input fields
+	 *
+	 * @access  public
+	 * @since   1.2
+	*/
+	public function download_settings( $download_id = 0 ) {
+
+		$rate = $this->get_product_rate( $download_id );
+
+?>
+		<p>
+			<strong><?php _e( 'Affiliate Rates:', 'affiliate-wp' ); ?></strong>
+		</p>
+
+		<p>
+			<label for="affwp_product_rate">
+				<input type="text" name="_affwp_edd_product_rate" id="affwp_product_rate" class="small-text" value="<?php echo esc_attr( $rate['rate'] ); ?>" />
+				<?php _e( 'Referral Rate', 'affiliate-wp' ); ?>
+			</label>
+		</p>
+
+		<p>
+			<label for="affwp_product_rate_type">
+				<select name="_affwp_edd_product_rate_type" id="affwp_product_rate_type">
+					<option value=""><?php _e( 'Default', 'affiliate-wp' ); ?></option>
+					<?php foreach( affwp_get_affiliate_rate_types() as $key => $option ) : ?>
+						<option value="<?php echo esc_attr( $key ); ?>"<?php selected( $key, $rate['type'] ); ?>><?php echo esc_html( $option ); ?></option>
+					<?php endforeach; ?>
+				</select>
+				<?php _e( 'Referral Type', 'affiliate-wp' ); ?>
+			</label>
+		</p>
+
+		<p><?php _e( 'These settings will be used to calculate affiliate earnings per-sale. Leave blank to use default affiliate rates.', 'affiliate-wp' ); ?></p>
+<?php
+	}
+
+	/**
+	 * Tells EDD to save our product settings
+	 *
+	 * @access  public
+	 * @since   1.2
+	 * @return  array
+	*/
+	public function download_save_fields( $fields = array() ) {
+		$fields[] = '_affwp_edd_product_rate';
+		$fields[] = '_affwp_edd_product_rate_type';
+		return $fields;
 	}
 
 }
