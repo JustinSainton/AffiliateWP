75fdb634d147ce4d19be5f542dfee061b0d236db
Andrew Munro <andrew@sumobi.com>
2014-07-07 20:23:23 +1200

    Add affwp_update_profile_settings hook for saving profile fields

--- includes/affiliate-functions.php
+++ includes/affiliate-functions.php
@@ -741,10 +741,10 @@ function affwp_update_profile_settings( $data = array() ) {
 	}
 
 	if ( ! empty( $data['payment_email'] ) && is_email( $data['payment_email'] ) ) {
-
 		affiliate_wp()->affiliates->update( $affiliate_id, array( 'payment_email' => $data['payment_email'] ) );
-
 	}
+
+	do_action( 'affwp_update_profile_settings', $data );
 
 	if ( ! empty( $_POST['affwp_action'] ) ) {
 		wp_redirect( add_query_arg( 'affwp_notice', 'profile-updated' ) ); exit;
