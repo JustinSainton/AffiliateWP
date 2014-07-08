0cec3a781896f40d3aea34472048cf4bfec13460
Pippin Williamson <pippin@pippinsplugins.com>
2014-07-08 11:07:00 -0500

    Introduce commission_rate() for #283

--- includes/integrations/class-edd.php
+++ includes/integrations/class-edd.php
@@ -25,6 +25,9 @@ class Affiliate_WP_EDD extends Affiliate_WP_Base {
 		add_action( 'edd_edit_discount_form_bottom', array( $this, 'discount_edit' ) );
 		add_action( 'edd_post_update_discount', array( $this, 'store_discount_affiliate' ), 10, 2 );
 		add_action( 'edd_post_insert_discount', array( $this, 'store_discount_affiliate' ), 10, 2 );
+	
+		// Integration with EDD commissions to adjust commission rates if a referral is present
+		add_filter( 'eddc_calc_commission_amount', array( $this, 'commission_rate' ), 10, 2 );
 	}
 
 	/**
@@ -273,6 +276,36 @@ class Affiliate_WP_EDD extends Affiliate_WP_Base {
 		$affiliate_id = affwp_get_affiliate_id( $user_id );
 
 		update_post_meta( $discount_id, 'affwp_discount_affiliate', $affiliate_id );
+	}
+
+	/**
+	 * Adjust the commission rate recorded if a referral is present
+	 *
+	 * @access  public
+	 * @since   1.2
+	*/
+	public function commission_rate( $amount, $args ) {
+
+		$referral_amount = affiliate_wp()->referrals->get_column_by( 'amount', 'reference', $args['payment_id']  );
+
+		if( ! $referral_amount ) {
+			return $amount;
+		}
+
+		if( 'flat' == $args['type'] ) {
+			return $args['rate'] - $referral_amount;
+		}
+
+		$args['price'] -= $referral_amount;
+
+		if ( $args['rate'] >= 1 ) {
+			$amount = $args['price'] * ( $args['rate'] / 100 ); // rate format = 10 for 10%
+		} else {
+			$amount = $args['price'] * $args['rate']; // rate format set as 0.10 for 10%
+		}
+
+
+		return $amount;
 	}
 
 }
