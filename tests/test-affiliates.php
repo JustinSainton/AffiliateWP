9768edb5f51ed5df491257d5008e85ee489069a5
Pippin Williamson <pippin@pippinsplugins.com>
2014-08-15 17:01:38 -0500

    Add basic test_get_affiliate_id() test. #339

--- tests/test-affiliates.php
+++ tests/test-affiliates.php
@@ -5,5 +5,9 @@ class Affiliate_Tests extends WP_UnitTestCase {
 	function test_is_affiliate() {
 		$this->assertFalse( affwp_is_affiliate() );
 	}
+
+	function test_get_affiliate_id() {
+		$this->assertFalse( affwp_is_affiliate( 0 ) );
+	}
 }
 
