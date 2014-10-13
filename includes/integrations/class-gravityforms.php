1d92106c67019a1df203e6eab263b20e4dc34ae2
Pippin Williamson <pippin@pippinsplugins.com>
2014-10-13 10:32:03 -0500

    Complete Gravity Forms integration. #110

--- includes/integrations/class-gravityforms.php
+++ includes/integrations/class-gravityforms.php
@@ -6,11 +6,12 @@ class Affiliate_WP_Gravity_Forms extends Affiliate_WP_Base {
 
 		$this->context = 'gravityforms';
 
-		add_action( 'gform_after_submission', array( $this, 'add_pending_referral' ), 10, 2 );
-		add_action( 'gform_entry_created', array( $this, 'mark_referral_complete' ), 10, 100 );
-		add_action( 'gform_update_payment_status', array( $this, 'mark_referral_complete' ), 10, 3 );
+		add_filter( 'gform_entry_created', array( $this, 'add_pending_referral' ), 10, 2 );
+		add_action( 'gform_post_payment_completed', array( $this, 'mark_referral_complete' ), 10, 2 );
+		add_action( 'gform_post_payment_refunded', array( $this, 'revoke_referral_on_refund' ), 10, 2 );
 
 		add_filter( 'affwp_referral_reference_column', array( $this, 'reference_link' ), 10, 2 );
+
 	}
 
 	public function add_pending_referral( $entry, $form ) {
@@ -23,7 +24,7 @@ class Affiliate_WP_Gravity_Forms extends Affiliate_WP_Base {
 			$entry     = GFFormsModel::get_lead( $entry['id'] );
 			$products  = GFCommon::get_product_fields( $form, $entry );
 			$total     = 0;
-			foreach ( $products['products'] as $key => $product ) {
+			foreach ( $products['products'] as $key => $product ) {	
 
 				$desc .= $product['name'];
 				if( $key + 1 < count( $products ) ) {
@@ -45,33 +46,37 @@ class Affiliate_WP_Gravity_Forms extends Affiliate_WP_Base {
 
 			$total += floatval( $products['shipping']['price'] );
 
-			$this->insert_pending_referral( $total, $entry['id'], $desc );
+			$referral_total = $this->calculate_referral_amount( $total, $entry['id'] );
+
+			$this->insert_pending_referral( $referral_total, $entry['id'], $desc );
 		
 		}
 
 	}
 
-	public function mark_referral_complete( $entry, $form ) {
-		echo '<pre>'; print_r( $entry ); echo '</pre>'; exit;
-		if( empty( $entry['payment_status'] ) ) {
-			return;
-		}
+	public function mark_referral_complete( $entry, $action ) {
+		
+		$this->complete_referral( $entry['id'] );
 
-		if( 'Approved' == $entry['payment_status'] || 'Active' == $entry['payment_status'] ) {
+		$referral = affiliate_wp()->referrals->get_by( 'reference', $entry['id'], $this->context );
+		$amount   = affwp_currency_filter( affwp_format_amount( $referral->amount ) );
+		$name     = affiliate_wp()->affiliates->get_affiliate_name( $referral->affiliate_id );
+		$note     = sprintf( __( 'Referral #%d for %s recorded for %s', 'affiliate-wp' ), $referral->referral_id, $amount, $name );
 
-			$this->complete_referral( $entry['id'] );
-	
-		}
-
+		GFFormsModel::add_note( $entry["id"], 0, 'AffiliateWP', $note );
 	}
 
-	public function mark_referral_complete_on_change( $lead_id, $property_value, $previous_value ) {
+	public function revoke_referral_on_refund( $entry, $action ) {
+		
+		$this->reject_referral( $entry['id'] );
 
-		if( $property_value != 'Paid' && $property_value != 'Approved' ) {
-			return;
-		}
+		$referral = affiliate_wp()->referrals->get_by( 'reference', $entry['id'], $this->context );
+		$amount   = affwp_currency_filter( affwp_format_amount( $referral->amount ) );
+		$name     = affiliate_wp()->affiliates->get_affiliate_name( $referral->affiliate_id );
+		$note     = sprintf( __( 'Referral #%d for %s for %s rejected', 'affiliate-wp' ), $referral->referral_id, $amount, $name );
 
-		$this->complete_referral( $lead_id );
+		GFFormsModel::add_note( $entry["id"], 0, 'AffiliateWP', $note );
+	
 	}
 
 	public function reference_link( $reference = 0, $referral ) {
