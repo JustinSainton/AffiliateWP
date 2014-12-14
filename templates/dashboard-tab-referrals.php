a74e07c1aa376ccf3d4a368f947f2a7f3b2bb30c
Pippin Williamson <pippin@pippinsplugins.com>
2014-12-14 17:24:30 -0600

    First pass at Referrals tab in affiliate area. #446

---
+++ templates/dashboard-tab-referrals.php
@@ -0,0 +1,60 @@
+<div id="affwp-affiliate-dashboard-referrals" class="affwp-tab-content">
+
+	<h4><?php _e( 'Referrals', 'affiliate-wp' ); ?></h4>
+
+	<?php
+	$per_page = 30;
+	$page     = get_query_var( 'paged' ) ? get_query_var( 'paged' ) : 1;
+	$referrals   = affiliate_wp()->referrals->get_referrals( array(
+		'number'       => $per_page,
+		'offset'       => $per_page * ( $page - 1 ),
+		'affiliate_id' => affwp_get_affiliate_id(),
+	) );
+	?>
+
+	<table id="affwp-affiliate-dashboard-referrals" class="affwp-table">
+		<thead>
+			<tr>
+				<th class="referral-amount"><?php _e( 'Amount', 'affiliate-wp' ); ?></th>
+				<th class="referral-description"><?php _e( 'Description', 'affiliate-wp' ); ?></th>
+				<th class="referral-status"><?php _e( 'Status', 'affiliate-wp' ); ?></th>
+				<th class="referral-date"><?php _e( 'Date', 'affiliate-wp' ); ?></th>
+			</tr>
+		</thead>
+
+		<tbody>
+			<?php if ( $referrals ) : ?>
+
+				<?php foreach ( $referrals as $referral ) : ?>
+					<tr>
+						<td><?php echo affwp_currency_filter( affwp_format_amount( $referral->amount ) ); ?></td>
+						<td><?php echo $referral->description; ?></td>
+						<td><?php echo $referral->status; ?></td>
+						<td><?php echo date_i18n( get_option( 'date_format' ), strtotime( $referral->date ) ); ?></td>
+					</tr>
+				<?php endforeach; ?>
+
+			<?php else : ?>
+
+				<tr>
+					<td colspan="3"><?php _e( 'You have not made any referrals yet.', 'affiliate-wp' ); ?></td>
+				</tr>
+
+			<?php endif; ?>
+		</tbody>
+	</table>
+
+	<div class="affwp-pagination">
+		<?php
+		echo paginate_links( array(
+			'current'      => $page,
+			'total'        => ceil( affwp_count_referrals( affwp_get_affiliate_id() ) / $per_page ),
+			'add_fragment' => '#affwp-affiliate-dashboard-referrals',
+			'add_args'     => array(
+				'tab'      => 'referrals'
+			)
+		) );
+		?>
+	</div>
+
+</div>
