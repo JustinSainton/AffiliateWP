90c4d9bd059314929696939074ec572b37874b63
Andrew Munro <andrew@sumobi.com>
2015-02-11 13:19:31 +1300

    update frontend JS

--- assets/js/frontend.js
+++ assets/js/frontend.js
@@ -1,20 +1,34 @@
 jQuery(document).ready( function($) {
-	$( '#affwp-generate-ref-url' ).submit( function() {
-		var url    = $( '#affwp-url' ).val(),
-		    refVar = $( '#affwp-referral-var' ).val(),
-		    affId  = $( '#affwp-affiliate-id' ).val();
 
-		if ( url.indexOf( '?' ) < 0 ) {
-			refVar = '?' + refVar;
-		} else {
-			refVar = '&' + refVar;
+	$( '#affwp-generate-ref-url' ).submit( function() {
+
+		var url       = $( '#affwp-url' ).val(),
+		    refVar    = $( '#affwp-referral-var' ).val(),
+		    affId     = $( '#affwp-affiliate-id' ).val(),
+		    refPretty = $( '#affwp-referral_pretty_urls' ).val(),
+		    refFormat = $( '#affwp-referral-format' ).val();
+
+		if ( ! url.match( /\/$/ ) ) {
+		    url += '/';
 		}
 
-		url = url + refVar + '=' + affId;
+		if ( refPretty ) {
+			url = url + refVar + '/' + affId;
+		} else {
+			// non-pretty
+			if ( url.indexOf( '?' ) < 0 ) {
+				refVar = '?' + refVar;
+			} else {
+				refVar = '&' + refVar;
+			}
+
+			url = url + refVar + '=' + affId;
+		}
 
 		$( '.affwp-referral-url-wrap' ).slideDown();
 		$( '#affwp-referral-url' ).val( url ).focus();
 
 		return false;
 	});
+
 });
