e883968c8e1e9319bb1133d18736957919c6334b
Ghost1227 <dgriffiths@section214.com>
2015-03-08 16:44:53 -0500

    Merge notification function

--- includes/emails/class-affwp-emails.php
+++ includes/emails/class-affwp-emails.php
@@ -289,6 +289,86 @@ class Affiliate_WP_Emails {
 
 
 	/**
+	 * Setup a notification
+	 *
+	 * @since 1.6
+	 * @param string $type The type of notification to send
+	 * @param array $args
+	 */
+	public function notification( $type = '', $args = array() ) {
+		if( empty( $type ) ) {
+			return false;
+		}
+
+		switch( $type ) {
+			case 'registration':
+				$email            = apply_filters( 'affwp_registration_admin_email', get_option( 'admin_email' ) );
+				$user_info        = get_userdata( affwp_get_affiliate_user_id( $args['affiliate_id'] ) );
+				$user_url         = $user_info->user_url;
+				$promotion_method = get_user_meta( affwp_get_affiliate_user_id( $args['affiliate_id'] ), 'affwp_promotion_method', true );
+
+				$subject  = affiliate_wp()->settings->get( 'registration_subject', __( 'New Affiliate Registration', 'affiliate-wp' ) );
+				$message  = affiliate_wp()->settings->get( 'registration_email', false );
+
+				if( ! $message ) {
+					$message  = __( 'A new affiliate has registered on your site, ', 'affiliate-wp' ) . home_url() . "\n\n";
+					$message .= sprintf( __( 'Name: %s', 'affiliate-wp' ), $args['name'] ) . "\n\n";
+
+					if( $user_url ) {
+						$message .= sprintf( __( 'Website URL: %s', 'affiliate-wp' ), esc_url( $user_url ) ) . "\n\n";
+					}
+
+					if( $promotion_method ) {
+						$message .= sprintf( __( 'Promotion method: %s', 'affiliate-wp' ), esc_attr( $promotion_method ) ) . "\n\n";
+					}
+
+					if( affiliate_wp()->settings->get( 'require_approval' ) ) {
+						$message .= sprintf( __( 'Review pending applications: %s', 'affiliate-wp' ), admin_url( 'admin.php?page=affiliate-wp-affiliates&status=pending' ) ) . "\n\n";
+					}
+				}
+
+				$subject = apply_filters( 'affwp_registration_subject', $subject, $args );
+				$message = apply_filters( 'affwp_registration_email', $message, $args );
+
+				break;
+			case 'application_accepted':
+				$email   = affwp_get_affiliate_email( $args['affiliate_id'] );
+				$subject = affiliate_wp()->settings->get( 'accepted_subject', __( 'Affiliate Application Accepted', 'affiliate-wp' ) );
+				$message = affiliate_wp()->settings->get( 'accepted_email', false );
+
+				if( ! $message ) {
+					$message  = sprintf( __( 'Contratulations %s!', 'affiliate-wp' ), affiliate_wp()->affiliates->get_affiliate_name( $args['affiliate_id'] ) ) . "\n\n";
+					$message .= sprintf( __( 'Your affiliate application on %s has been accepted!', 'affiliate-wp' ), home_url() ) . "\n\n";
+					$message .= sprintf( __( 'Log into your affiliate area at %s', 'affiliate-wp' ), affiliate_wp()->login->get_login_url() ) . "\n\n";
+				}
+				
+				$subject = apply_filters( 'affwp_application_accepted_subject', $subject, $args );
+				$message = apply_filters( 'affwp_application_accepted_email', $message, $args );
+
+				break;
+			case 'new_referral':
+				$email   = affwp_get_affiliate_email( $args['affiliate_id'] );
+				$subject = affiliate_wp()->settings->get( 'referral_subject', __( 'Referral Awarded!', 'affiliate-wp' ) );
+				$message = affiliate_wp()->settings->get( 'referral_email', false );
+				$amount  = html_entity_decode( affwp_currency_filter( $args['amount'] ), ENT_COMPAT, 'UTF-8' );
+
+				if( ! $message ) {
+					$message  = sprintf( __( 'Congratulations %s!', 'affiliate-wp' ), affiliate_wp()->affiliates->get_affiliate_name( $args['affiliate_id'] ) ) . "\n\n";
+					$message .= sprintf( __( 'You have been awarded a new referral of %s on %s!', 'affiliate-wp' ), $amount, home_url() ) . "\n\n";
+					$message .= sprintf( __( 'log into your affiliate area to view your earnings or disable these notifications: %s', 'affiliate-wp' ), affiliate_wp()->login->get_login_url() ) . "\n\n";
+				}
+
+				$subject = apply_filters( 'affwp_new_referral_subject', $subject, $args );
+				$message = apply_filters( 'affwp_new_referral_email', $message, $args );
+
+				break;
+		}
+
+		$this->send( $email, $subject, $message );
+	}
+
+
+	/**
 	 * Send the email
 	 *
 	 * @since 1.6
