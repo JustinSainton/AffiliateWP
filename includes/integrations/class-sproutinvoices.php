983945c7bdd884b0ee4247a39153bea5817da895
Dan Cameron <dan@sproutapps.co>
2015-03-23 13:30:00 -0700

    Fix from bad merge.

--- includes/integrations/class-sproutinvoices.php
+++ includes/integrations/class-sproutinvoices.php
@@ -24,10 +24,9 @@ class Affiliate_WP_Sprout_Invoices extends Affiliate_WP_Base {
 	}
 
 	public function mark_referral_complete( SI_Payment $payment ) {
-		
-		$this->complete_referral( $entry['id'] );
-
-		$referral = affiliate_wp()->referrals->get_by( 'reference', $entry['id'], $this->context );
+		$payment_id = $payment->get_id();
+		$this->complete_referral( $payment_id );
+		$referral = affiliate_wp()->referrals->get_by( 'reference', $payment_id, $this->context );
 		$amount   = affwp_currency_filter( affwp_format_amount( $referral->amount ) );
 		$name     = affiliate_wp()->affiliates->get_affiliate_name( $referral->affiliate_id );
 		$note     = sprintf( __( 'Referral #%d for %s recorded for %s', 'affiliate-wp' ), $referral->referral_id, $amount, $name );
