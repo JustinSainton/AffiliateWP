6399b4926424c1b71b0f3d99a95168f1648cf355
Pippin Williamson <pippin@pippinsplugins.com>
2015-03-27 16:50:48 -0500

    Corrected unit tests for #537

--- tests/test-affiliate-meta.php
+++ tests/test-affiliate-meta.php
@@ -18,27 +18,28 @@ class Affiliate_Meta_Tests extends WP_UnitTestCase {
 	}
 
 	function test_add_metadata() {
-		$this->assertFalse( affwp_add_affiliate_meta() );
-		$this->assertFalse( affwp_add_affiliate_meta( $this->_affiliate_id ) );
-		$this->assertNotEmpty( affwp_add_affiliate_meta( $this->_affiliate_id ), 'test_key' );
-		$this->assertNotEmpty( affwp_add_affiliate_meta( $this->_affiliate_id ), 'test_key', '1' );
+		$this->assertFalse( affwp_add_affiliate_meta( 0, '', '' ) );
+		$this->assertFalse( affwp_add_affiliate_meta( $this->_affiliate_id, '', '' ) );
+		$this->assertNotEmpty( affwp_add_affiliate_meta( $this->_affiliate_id, 'test_key' ) );
+		$this->assertNotEmpty( affwp_add_affiliate_meta( $this->_affiliate_id, 'test_key', '1' ) );
 	}
 
 	function test_update_metadata() {
-		$this->assertFalse( affwp_update_affiliate_meta() );
-		$this->assertFalse( affwp_update_affiliate_meta( $this->_affiliate_id ) );
-		$this->assertNotEmpty( affwp_update_affiliate_meta( $this->_affiliate_id ), 'test_key_2' );
-		$this->assertNotEmpty( affwp_update_affiliate_meta( $this->_affiliate_id ), 'test_key_2', '1' );
+		$this->assertEmpty( affwp_update_affiliate_meta() );
+		$this->assertEmpty( affwp_update_affiliate_meta( $this->_affiliate_id ) );
+		$this->assertNotEmpty( affwp_update_affiliate_meta( $this->_affiliate_id, 'test_key_2' ) );
+		$this->assertNotEmpty( affwp_update_affiliate_meta( $this->_affiliate_id, 'test_key_2', '1' ) );
 	}
 
 	function test_get_metadata() {
 		$this->assertFalse( affwp_get_affiliate_meta( $this->_affiliate_id ) );
-		$this->assertFalse( affwp_get_affiliate_meta( $this->_affiliate_id ), 'key_that_does_not_exist', true );
-		$this->assertEquals( '1', affwp_get_affiliate_meta( $this->_affiliate_id ), 'test_key_2', true );
-		$this->assertInternalType( 'array', affwp_get_affiliate_meta( $this->_affiliate_id ), 'test_key_2', false );
+		$this->assertFalse( affwp_get_affiliate_meta( $this->_affiliate_id, 'key_that_does_not_exist', true ) );
+		$this->assertEquals( '1', affwp_get_affiliate_meta( $this->_affiliate_id, 'test_key_2', true ) );
+		$this->assertInternalType( 'array', affwp_get_affiliate_meta( $this->_affiliate_id, 'test_key_2', false ) );
 	}
 
 	function test_delete_metadata() {
+		affwp_update_affiliate_meta( $this->_affiliate_id, 'test_key', '1' )
 		$this->assertTrue( affwp_delete_affiliate_meta( $this->_affiliate_id, 'test_key' ) );
 		$this->assertFalse( affwp_delete_affiliate_meta( $this->_affiliate_id, 'key_that_does_not_exist' ) );
 	}
