f7f88c1c0e8aaeae6397483a452e209b3579606f
Pippin Williamson <pippin@pippinsplugins.com>
2015-04-01 15:48:27 -0500

    Add tests for affwp_get_referral_status_label() and affwp_get_referral_status(). #484

--- tests/test-referrals.php
+++ tests/test-referrals.php
@@ -2,8 +2,42 @@
 
 class Referral_Tests extends WP_UnitTestCase {
 
+	protected $_affiliate_id = 0;
+	protected $_referral_id = 0;
+
+	function setUp() {
+		parent::setUp();
+
+		$args = array(
+			'user_id' => 1
+		);
+
+		$this->_affiliate_id = affiliate_wp()->affiliates->add( $args );
+
+
+		$args = array(
+			'affiliate_id' => 1,
+			'amount'    => 10,
+			'status'    => 'pending',
+			'context'   => 'tests'
+			'custom'    => 4,
+			'reference' => 5
+		);
+
+		$this->_referral_id = affiliate_wp()->referral->add( $args );
+
+	}
+
 	function test_get_referral() {
 		$this->assertNull( affwp_get_referral( 0 ) );
+	}
+
+	function test_get_referral_status() {
+		$this->assertEquals( 'pending', affwp_get_referral_status( $this->_referral_id ) );
+	}
+
+	function test_get_referral_status_label() {
+		$this->assertEquals( 'Pending', affwp_get_referral_status_label( $this->_referral_id ) );
 	}
 }
 
