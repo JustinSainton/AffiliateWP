5980f868dea7bad89f8db186b40504e0b6642c0e
Pippin Williamson <pippin@pippinsplugins.com>
2015-04-05 17:27:19 -0500

    Replace affiliate_wp_ with affwp_. #392

--- includes/emails/actions.php
+++ includes/emails/actions.php
@@ -20,12 +20,12 @@ if( ! defined( 'ABSPATH' ) ) exit;
  * @param array $args
  * @return void
  */
-function affiliate_wp_notify_on_registration( $affiliate_id = 0, $status = '', $args = array() ) {
+function affwp_notify_on_registration( $affiliate_id = 0, $status = '', $args = array() ) {
 	if( affiliate_wp()->settings->get( 'registration_notifications' ) ) {
 		affiliate_wp()->emails->notification( 'registration', array( 'affiliate_id' => $affiliate_id, 'name' => $args['display_name'] ) );
 	}
 }
-add_action( 'affwp_register_user', 'affiliate_wp_notify_on_registration', 10, 3 );
+add_action( 'affwp_register_user', 'affwp_notify_on_registration', 10, 3 );
 
 
 /**
@@ -36,14 +36,14 @@ add_action( 'affwp_register_user', 'affiliate_wp_notify_on_registration', 10, 3 
  * @param string $status
  * @param string $old_status
  */
-function affiliate_wp_notify_on_approval( $affiliate_id = 0, $status = '', $old_status = '' ) {
+function affwp_notify_on_approval( $affiliate_id = 0, $status = '', $old_status = '' ) {
 	if( 'active' != $status || 'pending' != $old_status ) {
 		return;
 	}
 
 	affiliate_wp()->emails->notification( 'application_accepted', array( 'affiliate_id' => $affiliate_id ) );
 }
-add_action( 'affwp_set_affiliate_status', 'affiliate_wp_notify_on_approval', 10, 3 );
+add_action( 'affwp_set_affiliate_status', 'affwp_notify_on_approval', 10, 3 );
 
 
 /**
@@ -53,7 +53,7 @@ add_action( 'affwp_set_affiliate_status', 'affiliate_wp_notify_on_approval', 10,
  * @param int $affiliate_id The ID of the registered affiliate
  * @param array $referral
  */
-function affiliate_wp_notify_on_new_referral( $affiliate_id = 0, $referral ) {
+function affwp_notify_on_new_referral( $affiliate_id = 0, $referral ) {
 	$user_id = affwp_get_affiliate_user_id( $affiliate_id );
 
 	if( ! get_user_meta( $user_id, 'affwp_referral_notifications', true ) ) {
@@ -62,4 +62,4 @@ function affiliate_wp_notify_on_new_referral( $affiliate_id = 0, $referral ) {
 
 	affiliate_wp()->emails->notification( 'new_referral', array( 'affiliate_id' => $affiliate_id, 'amount' => $referral->amount ) );
 }
-add_action( 'affwp_referral_accepted', 'affiliate_wp_notify_on_new_referral', 10, 2 );
+add_action( 'affwp_referral_accepted', 'affwp_notify_on_new_referral', 10, 2 );
