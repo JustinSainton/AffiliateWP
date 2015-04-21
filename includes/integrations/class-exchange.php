8b6c685a2b1166748d603ad6c61b563a88ca8f07
Andrew Munro <andrew@sumobi.com>
2015-04-21 11:21:57 -0500

    Allow affiliate to be removed from affiliate coupon field

--- includes/integrations/class-exchange.php
+++ includes/integrations/class-exchange.php
@@ -253,7 +253,12 @@ class Affiliate_WP_Exchange extends Affiliate_WP_Base {
 	 * @since   1.3
 	*/
 	public function store_coupon_affiliate( $coupon_id = 0, $data = array() ) {
-
+		
+		if ( empty( $_POST['user_name'] ) ) {		
+			delete_post_meta( $coupon_id, 'affwp_coupon_affiliate' );
+			return;
+		}
+		
 		if( empty( $_POST['user_id'] ) && empty( $_POST['user_name'] ) ) {
 			return;
 		}
