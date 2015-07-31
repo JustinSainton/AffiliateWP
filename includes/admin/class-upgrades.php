fc42861de838ce4e59f8629d85012c22580c4b8a
Frankie Jarrett <frankie@x-team.com>
2015-07-31 10:09:55 -0500

    Use blog-specific prefixes when upgrading NF tables

--- includes/admin/class-upgrades.php
+++ includes/admin/class-upgrades.php
@@ -196,20 +196,20 @@ class Affiliate_WP_Upgrades {
 
 		global $wpdb;
 
-		$tables = $wpdb->get_var( "SHOW TABLES LIKE '{$wpdb->base_prefix}nf_object%';" );
+		$tables = $wpdb->get_var( "SHOW TABLES LIKE '{$wpdb->prefix}nf_object%';" );
 
 		if ( ! $tables ) {
 			return;
 		}
 
-		$forms = $wpdb->get_results( "SELECT id FROM {$wpdb->base_prefix}nf_objects WHERE type = 'form';" );
+		$forms = $wpdb->get_results( "SELECT id FROM {$wpdb->prefix}nf_objects WHERE type = 'form';" );
 
 		if ( ! $forms ) {
 			return;
 		}
 
 		// There could be forms that already have this meta saved in the DB, we will ignore those
-		$_forms = $wpdb->get_results( "SELECT object_id FROM {$wpdb->base_prefix}nf_objectmeta WHERE meta_key = 'affwp_allow_referrals';" );
+		$_forms = $wpdb->get_results( "SELECT object_id FROM {$wpdb->prefix}nf_objectmeta WHERE meta_key = 'affwp_allow_referrals';" );
 
 		$forms  = wp_list_pluck( $forms, 'id' );
 		$_forms = wp_list_pluck( $_forms, 'object_id' );
@@ -223,7 +223,7 @@ class Affiliate_WP_Upgrades {
 
 			$wpdb->query(
 				$wpdb->prepare(
-					"INSERT INTO {$wpdb->base_prefix}nf_objectmeta (object_id,meta_key,meta_value) VALUES (%d,'affwp_allow_referrals','1');",
+					"INSERT INTO {$wpdb->prefix}nf_objectmeta (object_id,meta_key,meta_value) VALUES (%d,'affwp_allow_referrals','1');",
 					$form_id
 				)
 			);
