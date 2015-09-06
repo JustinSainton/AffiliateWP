945220ce212df074d3cc5e7555d1a287c5588fde
Pippin Williamson <pippin@pippinsplugins.com>
2015-09-06 07:34:17 -0500

    Strip off fragment identifiers. #811

--- assets/js/tracking.js
+++ assets/js/tracking.js
@@ -86,6 +86,10 @@ jQuery(document).ready( function($) {
         for(var i = 0; i < hashes.length; i++) {
             hash = hashes[i].split('=');
             vars.push(hash[0]);
+
+            // Remove fragment identifiers
+            var n = hash[1].indexOf('#');
+            hash[1] = hash[1].substring(0, n != -1 ? n : hash[1].length);
             vars[hash[0]] = hash[1];
         }
         return vars;
