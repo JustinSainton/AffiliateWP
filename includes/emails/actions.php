f9b3b95789a9ce463c1e4e632abf23744292123d
Andrew Munro <andrew@sumobi.com>
2015-10-26 10:52:54 +1300

    Fix comment

--- includes/emails/actions.php
+++ includes/emails/actions.php
@@ -86,9 +86,11 @@ function affwp_notify_on_approval( $affiliate_id = 0, $status = '', $old_status 
 		return;
 	}
 
-	// only send email if:
-	// affiliate approval is disabled ( $status = active, $old_status = active )
-	// affiliate approval is enabled and the affiliate is accepted ( $status = active, $old_status = pending )
+	/**
+	 * Only send email if:
+	 * Affiliate approval is disabled ( $status = active, $old_status = active )
+	 * Affiliate approval is enabled and the affiliate is accepted ( $status = active, $old_status = pending )
+	 */
 	if ( ! ( 'active' == $status && 'active' == $old_status || 'active' == $status && 'pending' == $old_status ) ) {
 		return;
 	}
