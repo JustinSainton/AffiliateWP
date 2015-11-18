85f93aab558b237f9125611e3a20bfa2c9c76428
Andrew Munro <andrew@sumobi.com>
2015-11-19 00:04:03 +1300

    check for guest email address

--- includes/integrations/class-exchange.php
+++ includes/integrations/class-exchange.php
@@ -78,7 +78,16 @@ class Affiliate_WP_Exchange extends Affiliate_WP_Base {
 
 		if( $this->affiliate_id ) {
 
-			if ( $this->is_affiliate_email( $this->transaction->shipping_address['email'] ) ) {
+			$guest_checkout_email = it_exchange_get_cart_data( 'guest-checkout-user' );
+			$guest_checkout_email = is_array( $guest_checkout_email ) ? reset( $guest_checkout_email ) : $guest_checkout_email;
+
+			if ( $guest_checkout_email ) {
+				$email = $guest_checkout_email;
+			} else {
+				$email = $this->transaction->shipping_address['email'];
+			}
+
+			if ( $this->is_affiliate_email( $email ) ) {
 				return; // Customers cannot refer themselves
 			}
 
