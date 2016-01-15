4daca8911b586e296fc72d4debdb3f63f2de75d0
Pippin Williamson <pippin@pippinsplugins.com>
2016-01-15 15:43:36 +1300

    URLdecode all the URLs. #1054

--- templates/dashboard-tab-urls.php
+++ templates/dashboard-tab-urls.php
@@ -14,7 +14,7 @@
 	<form id="affwp-generate-ref-url" class="affwp-form" method="get" action="#affwp-generate-ref-url">
 		<div class="affwp-wrap affwp-base-url-wrap">
 			<label for="affwp-url"><?php _e( 'Page URL', 'affiliate-wp' ); ?></label>
-			<input type="text" name="url" id="affwp-url" value="<?php echo esc_url( affwp_get_affiliate_base_url() ); ?>" />
+			<input type="text" name="url" id="affwp-url" value="<?php echo esc_url( urldecode( affwp_get_affiliate_base_url() ) ); ?>" />
 		</div>
 
 		<div class="affwp-wrap affwp-campaign-wrap">
@@ -24,7 +24,7 @@
 
 		<div class="affwp-wrap affwp-referral-url-wrap" <?php if ( ! isset( $_GET['url'] ) ) { echo 'style="display:none;"'; } ?>>
 			<label for="affwp-referral-url"><?php _e( 'Referral URL', 'affiliate-wp' ); ?></label>
-			<input type="text" id="affwp-referral-url" value="<?php echo esc_url( affwp_get_affiliate_referral_url() ); ?>" />
+			<input type="text" id="affwp-referral-url" value="<?php echo esc_url( urldecode( affwp_get_affiliate_referral_url() ) ); ?>" />
 			<div class="description"><?php _e( '(now copy this referral link and share it anywhere)', 'affiliate-wp' ); ?></div>
 		</div>
 
