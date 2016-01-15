e7b381d60af671478316d9acbd7eab4fa824566a
Pippin Williamson <pippin@pippinsplugins.com>
2016-01-15 13:55:57 +1300

    Add test_long_campaign() test. #1055

--- tests/test-visits.php
+++ tests/test-visits.php
@@ -6,5 +6,17 @@ class Visit_Tests extends WP_UnitTestCase {
 		$this->assertFalse( false );
 	}
 
+	function test_long_campaign() {
+
+		// The 2 should get trimmed off as it is the 51st character
+		$campaign = '111111111111111111111111111111111111111111111111112';
+		$visit_id = affiliate_wp()->visits->add( array( 'campaign' => $campaign, 'affiliate_id' => 1 ) );
+		$visit    = affiliate_wp()->visits->get( $visit_id );
+
+		$this->assertEqual( 50, strlen( $visit->campaign ) );
+		$this->assertEqual( '11111111111111111111111111111111111111111111111111', $visit->campaign );
+
+	}
+
 }
 
